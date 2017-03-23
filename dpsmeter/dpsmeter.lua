32ea49c4655e40586c9a6cbdef4213f1a864dc45
= <dreamfragment623+weizlogy@gmail.com>
2017-03-24 05:55:25 +0900

    show ui and max dps.\

--- dpsmeter/dpsmeter.lua
+++ dpsmeter/dpsmeter.lua
@@ -1,5 +1,6 @@
 DPSMeter = {};
 DPSMeter.Scores = {};
+DPSMeter.IsLock = 0;
 
 -- constructor.
 function DPSMeter.new(self)
@@ -9,6 +10,7 @@ function DPSMeter.new(self)
   members.timeElapsed = 0;
   members.hpBased = 0;
   members.hpElapsed = 0;
+  members.maxdps = 0;
   members.name = "";
   -- start count.
   members.Start = function(self, handle)
@@ -16,23 +18,52 @@ function DPSMeter.new(self)
     self.timeBased = imcTime.GetAppTimeMS();
     self.hpBased = stat.HP;
     self.name = info.GetName(handle);
-  end
-  -- stop count.
-  members.Stop = function(self, handle)
-    if (self.hpElapsed == 0) then
-      return;
+
+    local frameName = "dpsmeter_" .. handle;
+    local frame = ui.GetFrame(frameName);
+    if (frame == nil) then
+      frame = ui.CreateNewFrame("dpsmeter", frameName, 0);
     end
-    local dps = self.hpElapsed / (self.timeElapsed / 1000);
-    CHAT_SYSTEM(string.format("%d dps on %s", dps, self.name));
+    frame:SetLayerLevel(1);
+    frame:Resize(200, 20);
+    frame:ShowWindow(1);
+
+    local dpstext = frame:CreateOrGetControl('richtext', "dpstext", 0, 0, 200, 20);
+    dpstext:ShowWindow(1);
+
+    local w, h = 0;
+    FRAME_AUTO_POS_TO_OBJ(frame, handle, w, h, 1, 1, 1);
+    frame:RunUpdateScript("DPSMETER_UPDATE");
   end
   -- update count.
-  members.Update = function(self, handle)
+  members.Update = function(self, frame, handle)
 		local stat = info.GetStat(handle);
+    local dpstext = GET_CHILD(frame, "dpstext", "ui::CRichText");		
+
+    local distFromActor = info.GetTargetInfo(handle).distance;
+    if (distFromActor > 130) then
+      self.timeBased = imcTime.GetAppTimeMS();
+      self.hpBased = stat.HP;
+      dpstext:SetColorTone("99FFFFFF");
+      return 1;
+    end
     self.timeElapsed = self.timeElapsed + (imcTime.GetAppTimeMS() - self.timeBased);
     self.hpElapsed = self.hpElapsed + (self.hpBased - stat.HP);
-  end
-  -- clear create objects.
-  members.Clear = function(self)
+    local dps = self.hpElapsed / (self.timeElapsed / 1000);
+
+    self.maxdps = math.max(self.maxdps, dps);
+    dpstext:SetColorTone("FFFFFFFF");
+    dpstext:SetText(string.format("{ol}%.2f(%.2f) dps", dps, self.maxdps));
+
+    local isDead = world.GetActor(handle):IsDead();
+    if (isDead == 1) then
+      CHAT_SYSTEM(string.format("max %.2f dps on %s", self.maxdps, self.name));
+      --ui.DestroyFrame(frame:GetName());
+      local score = DPSMeter.Scores[handle];
+      DPSMeter.Scores[handle] = nil;
+      return 0;
+    end
+    return 1;
   end
   return setmetatable(members, {__index = self});
 end
@@ -40,36 +71,37 @@ end
 setmetatable(DPSMeter, {__call = DPSMeter.new});
 -- frame initialize.
 function DPSMETER_ON_INIT(addon, frame)
-  -- for normal monsters.
-	addon:RegisterMsg('TARGET_SET', 'DPSMETER_START');
-	addon:RegisterMsg('TARGET_CLOSE', 'DPSMETER_STOP');
-	addon:RegisterMsg('TARGET_CLEAR', 'DPSMETER_STOP');
-	addon:RegisterMsg('TARGET_UPDATE', 'DPSMETER_UPDATE');
-  -- for bosses.
-	addon:RegisterMsg('TARGET_SET_BOSS', 'DPSMETER_START');
-	addon:RegisterMsg('TARGET_CLEAR_BOSS', 'DPSMETER_STOP');
+  addon:RegisterMsg('MAP_CHARACTER_UPDATE', 'DPSMETER_START');
 end
-function DPSMETER_START(frame, msg, str, num)
-  local handle = session.GetTargetHandle();
-  local frame = ui.GetFrame("monb_" .. handle);
-  if (frame == nil) then
+function DPSMETER_START(frame, msg, str, myhandle)
+  if (DPSMeter.IsLock == 1) then
     return;
   end
-  local score = DPSMeter.Scores[handle];
-  if (score == nil) then
-    score = DPSMeter();
-    DPSMeter.Scores[handle] = score;
+  DPSMeter.IsLock = 1;
+  local list, count = SelectBaseObject(GetMyPCObject(), 500, "ALL");
+  for i = 1 , count do
+    local obj = list[i];
+    local iesObj = GetBaseObjectIES(obj);
+    local actor = tolua.cast(obj, "CFSMActor");
+    local handle = actor:GetHandleVal();
+    -- do nothing for my pc.
+    if (myhandle ~= handle) then
+      local objType = actor:GetObjType();
+      local faction = actor:GetFactionStr();
+      if (objType == GT_MONSTER and faction == "Monster") then
+        local score = DPSMeter.Scores[handle];
+        if (score == nil) then
+          score = DPSMeter();
+          DPSMeter.Scores[handle] = score;
+        end
+        score:Start(handle);
+      end
+    end
   end
-  score:Start(handle);
+  DPSMeter.IsLock = 0;
 end
-function DPSMETER_STOP(frame, msg, str, handle)
+function DPSMETER_UPDATE(frame)
+  local handle = frame:GetUserIValue("_AT_OFFSET_HANDLE");    -- Setting by FRAME_AUTO_POS_TO_OBJ
   local score = DPSMeter.Scores[handle];
-  score:Stop(handle);
-  score:Clear();
-  DPSMeter.Scores[handle] = nil;
-end
-function DPSMETER_UPDATE(frame, msg, str, num)
-  local handle = session.GetTargetHandle();
-  local score = DPSMeter.Scores[handle];
-  score:Update(handle);
+  return score:Update(frame, handle);
 end
