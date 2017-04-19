b2e1bde04eb4f07b4f5ee3f7d4816a22e12ee33f
= <dreamfragment623+weizlogy@gmail.com>
2017-04-20 05:21:43 +0900

    v1.3.0 dpsmeter.

--- dpsmeter/dpsmeter.lua
+++ dpsmeter/dpsmeter.lua
@@ -1,11 +1,13 @@
 DPSMeter = {};
 DPSMeter.Scores = {};
 DPSMeter.IsLock = 0;
+DPSMeter.IgnoreChat = 1;
 
 -- constructor.
 function DPSMeter.new(self)
   -- initialize members.
   local members = {};
+  -- dps caclulate values.
   members.timeBased = 0;
   members.timeElapsed = 0;
   members.hpBased = 0;
@@ -13,6 +15,8 @@ function DPSMeter.new(self)
   members.maxdps = 0;
   members.name = "";
   members.counting = 0;
+  members.totaldamage = 0;
+  members.beforeHp = 0;
   -- start count.
   members.Start = function(self, handle)
     -- show UI.
@@ -22,11 +26,14 @@ function DPSMeter.new(self)
       frame = ui.CreateNewFrame("dpsmeter", frameName, 0);
     end
     frame:SetLayerLevel(1);
-    frame:Resize(200, 20);
+    frame:Resize(500, 20);
     frame:ShowWindow(1);
 
-    local dpstext = frame:CreateOrGetControl('richtext', "dpstext", 0, 0, 200, 20);
+    local dpstext = frame:CreateOrGetControl('richtext', "dpstext", 0, 0, frame:GetWidth(), frame:GetHeight());
     dpstext:ShowWindow(0);
+    dpstext:EnableHitTest(1);
+    dpstext:SetEventScriptArgNumber(ui.RBUTTONUP, handle);
+    dpstext:SetEventScript(ui.RBUTTONUP, 'DPSMETER_RBUP_MENU');
 
     local w, h = 0;
     FRAME_AUTO_POS_TO_OBJ(frame, handle, w, h, 1, 1, 1);
@@ -45,6 +52,7 @@ function DPSMeter.new(self)
       self.timeBased = imcTime.GetAppTimeMS() - 1000;
       self.hpBased = stat.maxHP;
       self.name = info.GetName(handle);
+      self.beforeHp = stat.maxHP;
     end
     -- calculate dps.
     local dpstext = GET_CHILD(frame, "dpstext", "ui::CRichText");		
@@ -54,13 +62,23 @@ function DPSMeter.new(self)
     local dps = self.hpElapsed / (self.timeElapsed / 1000);
     -- save max dps.
     self.maxdps = math.max(self.maxdps, dps);
+    -- calculate current average, if hp changed.
+    if (self.beforeHp ~= stat.HP) then
+      self.counting = self.counting + 1;
+      self.totaldamage = self.totaldamage + dps;
+      self.beforeHp = stat.HP;
+    end
+    local average = self.totaldamage / self.counting;
     -- show dps.
-    dpstext:SetColorTone("FFFFFFFF");
-    dpstext:SetText(string.format("{ol}%.2f(%.2f) dps", dps, self.maxdps));
+    --dpstext:SetColorTone("FFFFFFFF");
+    dpstext:SetText(string.format("{ol}%.2f[{#99ffff}%.2f][{#ff9999}%.2f] dps", dps, average, self.maxdps));
     -- target dead -> show max dps to chat.
     local isDead = world.GetActor(handle):IsDead();
     if (isDead == 1) then
-      CHAT_SYSTEM(string.format("[dpsmeter] max %5.2f dps - %s", self.maxdps, self.name));
+      if (DPSMeter.IgnoreChat == 0) then
+        CHAT_SYSTEM(string.format(
+          "[dpsmeter] max %5.2f, average %5.2f dps - %s", self.maxdps, average, self.name));
+      end
       --ui.DestroyFrame(frame:GetName());
       local score = DPSMeter.Scores[handle];
       DPSMeter.Scores[handle] = nil;
@@ -68,12 +86,16 @@ function DPSMeter.new(self)
     end
     return 1;
   end
+  members.toggleChat = function(self, handle)
+
+  end
   return setmetatable(members, {__index = self});
 end
 -- set call.
 setmetatable(DPSMeter, {__call = DPSMeter.new});
 -- frame initialize.
 function DPSMETER_ON_INIT(addon, frame)
+  -- register handlers.
   addon:RegisterMsg('MAP_CHARACTER_UPDATE', 'DPSMETER_START');
   addon:RegisterMsg('FPS_UPDATE', 'DPSMETER_START');
 end
@@ -92,7 +114,7 @@ function DPSMETER_START(frame, msg, str, myhandle)
     if (myhandle ~= handle) then
       local objType = actor:GetObjType();
       local faction = actor:GetFactionStr();
-      if (objType == GT_MONSTER and faction ~= "Pet") then
+      if (objType == GT_MONSTER and faction ~= "Pet" and faction ~= "Summon") then
         local score = DPSMeter.Scores[handle];
         if (score == nil) then
           score = DPSMeter();
@@ -108,4 +130,20 @@ function DPSMETER_UPDATE(frame)
   local handle = frame:GetUserIValue("_AT_OFFSET_HANDLE");    -- Setting by FRAME_AUTO_POS_TO_OBJ
   local score = DPSMeter.Scores[handle];
   return score:Update(frame, handle);
+end
+function DPSMETER_RBUP_MENU(frame, ctrl, str, num)
+  local handle = num;
+  local menuTitle = string.format("[%d] %s", handle, "dpsmeter");
+  local context = ui.CreateContextMenu(
+    "CONTEXT_DPSMETER", menuTitle, 0, 0, string.len(menuTitle) * 10, 100);
+  ui.AddContextMenuItem(context, "toggleChat", string.format("DPSMETER_TOGGLE_CHAT(%d)", handle));	
+  ui.AddContextMenuItem(context, "Cancel", "None");
+  ui.OpenContextMenu(context);
+end
+function DPSMETER_TOGGLE_CHAT(handle)
+  if (DPSMeter.IgnoreChat == 1) then
+    DPSMeter.IgnoreChat = 0;
+    return;
+  end
+  DPSMeter.IgnoreChat = 1;
 end
