3aafb0284ced41c78ae7e5e59196eb72826025d1
= <dreamfragment623+weizlogy@gmail.com>
2017-04-29 08:34:22 +0900

    fixed #7. improved #5.

--- worldmapquest/worldmapquest.lua
+++ worldmapquest/worldmapquest.lua
@@ -6,6 +6,7 @@ function WorldmapQuest.new(self)
   -- initialize members.
   local members = {};
   members.possibleQuests = {};
+  members.IgnoreRepeatQuest = 0;
   --
   members.CreatePossibleQuests = function(self)
     self.possibleQuests = {}
@@ -19,18 +20,33 @@ function WorldmapQuest.new(self)
           count = 0;
         end
         self.possibleQuests[mapCls.ClassID] = count + 1;
+        if (self.IgnoreRepeatQuest == 1 and questIES.QuestMode == "REPEAT") then
+          self.possibleQuests[mapCls.ClassID] = count - 1;
+        end
       end
     end
   end
   --
-  members.InsertPossibleQuestCount = function(self, mapCls, textObj)
-    local value = textObj:GetTextByKey("value");
+  members.InsertPossibleQuestCount = function(self, mapCls, gbox, parentGBox)
+    parentGBox:RemoveChild("questText_"..mapCls.ClassID);
     local count = self.possibleQuests[mapCls.ClassID];
-    if (count == nil) then
+    if (count == nil or count <= 0) then
       return;
     end
-    textObj:SetTextByKey(
-      "value", string.format("{@st66e}[%d]", count).."{nl}"..value);
+    local questText = parentGBox:CreateOrGetControl(
+      "richtext", "questText_"..mapCls.ClassID, gbox:GetX(), gbox:GetY(), 30, 30);
+    questText:SetText(string.format("{@st66e}[%d]", count));
+    questText:SetOffset(gbox:GetOffsetX() + (gbox:GetWidth() / 2) - 14, gbox:GetOffsetY() - 20);
+    questText:EnableHitTest(1);
+    questText:SetEventScript(ui.RBUTTONUP, 'WORLDMAPQUEST_RBUP_MENU');
+  end
+  --
+  members.ToggleRepeatQuest = function(self)
+    if (self.IgnoreRepeatQuest == 1) then
+      self.IgnoreRepeatQuest = 0;
+      return;
+    end
+    self.IgnoreRepeatQuest = 1;
   end
   -- destroy.
   members.Destroy = function(self)
@@ -70,12 +86,22 @@ function WORLDMAPQUEST_ON_INIT(addon, frame)
       x, spaceX, startX, y, spaceY, startY, pictureStartY);
     -- create params.
     local gbox = GET_CHILD(parentGBox, gBoxName, "ui::CGroupBox");
-    local ctrlSet = GET_CHILD(
-      gbox, "ZONE_CTRL_" .. mapCls.ClassID, "ui::CControlSet");
-    local text = ctrlSet:GetChild("text");
     -- call interrupt function.
-    woqu:InsertPossibleQuestCount(mapCls, text);
+    woqu:InsertPossibleQuestCount(mapCls, gbox, parentGBox);
   end
+end
+--
+function WORLDMAPQUEST_RBUP_MENU(frame, ctrl, str, num)
+  local menuTitle = "worldmapquest";
+  local context = ui.CreateContextMenu(
+    "CONTEXT_WORLDMAPQUEST", menuTitle, 0, 0, string.len(menuTitle) * 10, 100);
+  ui.AddContextMenuItem(context, "toggleRepeatQuest", "WORLDMAPQUEST_TOGGLE_REPEAT_QUEST()");	
+  ui.AddContextMenuItem(context, "Cancel", "None");
+  ui.OpenContextMenu(context);
+end
+--
+function WORLDMAPQUEST_TOGGLE_REPEAT_QUEST()
+  woqu:ToggleRepeatQuest();
 end
 
 -- create instance.
