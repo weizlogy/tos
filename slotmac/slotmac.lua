75182e0da79ea1451a05a357b3654fdd0178a55f
= <dreamfragment623+weizlogy@gmail.com>
2017-06-30 23:39:32 +0900

    add overheat macro.

--- slotmac/slotmac.lua
+++ slotmac/slotmac.lua
@@ -28,9 +28,8 @@ function SlotMac.new(self)
 
   -- analyze, too long code...
   members.AnalyzeMacro = function(self, input)
-    local cmds = {
-      act = function() end
-    };
+    local cmds = MacroCommand();
+    cmds.act = cmds.NoOp;
     -- extract emoticon.
     -- <emoticon_nnnn>
     local extra = string.gsub(input, "<(emoticon\_.-)>", "{img %1 0 0}{/}");
@@ -41,10 +40,8 @@ function SlotMac.new(self)
     -- chat alias.
     -- /xxx
     if (string.find(input, "/", 1, true) == 1) then
-      cmds.act = function(frame, slot, argStr, argNum)
-        local temp = string.gsub(input, "^\/s ", "");  -- for normal chat.
-        ui.Chat(temp);
-      end
+      cmds.cc = input;
+      cmds.act = cmds.ChatAlias;
       return cmds;
     end
     -- analyze custom commands.
@@ -52,48 +49,33 @@ function SlotMac.new(self)
     cmds.id = id;
     cmds.cc = cc;
     --CHAT_SYSTEM(id.." - "..cc);
+
     -- slot skill.
     -- #skill# nnn
     if (id == "skill") then
-      cmds.act = function(frame, slot, argStr, argNum)
-        if (cmds.cc ~= "0") then
-          -- use other slot skill.
-          local frame = ui.GetFrame('quickslotnexpbar');
-		      slot = GET_CHILD_RECURSIVELY(frame, "slot"..cmds.cc, "ui::CSlot");
-        end
-        slmc.QUICKSLOTNEXPBAR_SLOT_USE(frame, slot, argStr, argNum);
-      end
+      cmds.act = cmds.Skill;
       return cmds;
     end
     -- system msg.
     -- #system# xxx
     if (id == "system") then
-      cmds.act = function(frame, slot, argStr, argNum)
-        CHAT_SYSTEM(cmds.cc);
-      end
+      cmds.act = cmds.System;
       return cmds;
     end
     -- pose.
     -- #pose# xxx
     if (id == "pose") then
-      cmds.act = function(frame, slot, argStr, argNum)
-        control.Pose(cmds.cc);
-      end
+      cmds.act = cmds.Pose;
       return cmds;
     end
     -- timer.
     -- #timer# nnn xxx
     if (id == "timer") then
-      cmds.act = function(frame, slot, argStr, argNum)
-        local time, timedcmd = string.match(cmds.cc, "^(%d+) (.+)$");
-        timedcmd = self:AnalyzeMacro(timedcmd);
-        local timeHandleName = "SLOTMAC_TIMER_"..IMCRandom(1, 100000);
-        _G[timeHandleName] = function()
-          timedcmd.act(frame, slot, argStr, argNum);
-          _G[timeHandleName] = nil;
-        end
-        DebounceScript(timeHandleName, time);
-      end
+      local time, timedcmd = string.match(cmds.cc, "^(%d+) (.+)$");
+      timedcmd = self:AnalyzeMacro(timedcmd);
+      cmds.cc = timedcmd;
+      cmds.dd = time;
+      cmds.act = cmds.Timer;
       return cmds;
     end
     -- equip.
@@ -133,20 +115,17 @@ function SlotMac.new(self)
           end
         end
       end
-      cmds.act = function(frame, slot, argStr, argNum)
-        --CHAT_SYSTEM(cmds.cc)
-        local slotName, className = string.match(cmds.cc, "^(.-) (.-)$");
-        local invItem = session.GetInvItemByName(className);
-        item.Equip(slotName, invItem.invIndex);
+      if (converted == 0) then
+        CHAT_SYSTEM(string.format("[slotmac] equip item [%s] is not found.", tempItemName));
+        return cmds;
       end
+      cmds.act = cmds.Equip;
       return cmds;
     end
     -- unequip.
     -- #unequip# xxx
     if (id == "unequip") then
-      cmds.act = function(frame, slot, argStr, argNum)
-        item.UnEquip(item.GetEquipSpotNum(cmds.cc));
-      end
+      cmds.act = cmds.UnEquip;
       return cmds;
     end
 
@@ -158,6 +137,10 @@ function SlotMac.new(self)
   -- load macro from file.
   members.UseMacro = function(self, frame, slot, argStr, argNum)
     local name = slot:GetName();
+    local oh = self:WillSkillOverHeat(slot);
+    if (oh ~= 0) then
+      name = name.."-oh";
+    end
     --CHAT_SYSTEM(name);
     local macro = SlotMac.List[name];
     if (macro == nil) then
@@ -165,8 +148,30 @@ function SlotMac.new(self)
       return;
     end
     for i, m in ipairs(macro) do
-      m.act(frame, slot, argStr, argNum);
+      m.act(m, frame, slot, argStr, argNum);
     end
+  end
+
+  members.HasSkillOverHeat = function(self, slot)
+    local obj = GET_SLOT_SKILL_OBJ(slot);
+    if (obj == nil or obj.OverHeatGroup == "None") then
+      return 0;
+    end
+    return 1;
+  end
+
+  members.WillSkillOverHeat = function(self, slot)
+    local obj = GET_SLOT_SKILL_OBJ(slot);
+    if (obj == nil or obj.OverHeatGroup == "None") then
+      return 0;
+    end
+    local sklType = obj.ClassID;
+    local skl = session.GetSkill(sklType);
+    skl = GetIES(skl:GetObject());
+    local useOverHeat = skl.SklUseOverHeat;
+    local curHeat = session.GetSklOverHeat(sklType) + useOverHeat;
+	  local maxOverHeat = session.GetSklMaxOverHeat(sklType);
+    return maxOverHeat - curHeat;
   end
 
   members.Destroy = function(self)
@@ -205,14 +210,24 @@ function SLOTMAC_ON_INIT(addon, frame)
     reload:SetEventScript(ui.RBUTTONUP, "SLOTMAC_CREATEDIR");
     reload:ShowWindow(1);
   end
-  SLOTMAC_RELOAD();
+  addon:RegisterMsg('GAME_START_3SEC', 'SLOTMAC_LOAD');
+end
+
+function SLOTMAC_LOAD(frame)
+  SLOTMAC_RELOAD(frame, nil, nil, nil);
 end
 
 function SLOTMAC_RELOAD(parent, sender, argStr, argNum)
+  SlotMac.List = {};
   local frame = ui.GetFrame('quickslotnexpbar');
   for i = 0, MAX_QUICKSLOT_CNT - 1 do
 		local slot = GET_CHILD_RECURSIVELY(frame, "slot"..(i + 1), "ui::CSlot");
     slmc:LoadMacro(slot:GetName());
+    -- check overheat.
+    local oh = slmc:HasSkillOverHeat(slot);
+    if (oh == 1) then
+      slmc:LoadMacro(slot:GetName().."-oh");
+    end
   end
   if (argStr ~= nil) then
     local cid = info.GetCID(session.GetMyHandle());
@@ -233,3 +248,60 @@ end
 
 -- create instance.
 slmc = SlotMac();
+
+
+-- sub classes.
+MacroCommand = {}
+function MacroCommand.new(self)
+  local members = {};
+  members.id = "";
+  members.cc = "";
+
+  members.NoOp = function(self, frame, slot, argStr, argNum)
+  end
+
+  members.Skill = function(self, frame, slot, argStr, argNum)
+    if (self.cc ~= "0") then
+      -- use other slot skill.
+      local frame = ui.GetFrame('quickslotnexpbar');
+      slot = GET_CHILD_RECURSIVELY(frame, "slot"..self.cc, "ui::CSlot");
+    end
+    slmc.QUICKSLOTNEXPBAR_SLOT_USE(frame, slot, argStr, argNum);
+  end
+
+  members.ChatAlias = function(self, frame, slot, argStr, argNum)
+    local temp = string.gsub(self.cc, "^\/s ", "");  -- for normal chat.
+    ui.Chat(temp);
+  end
+
+  members.System = function(self, frame, slot, argStr, argNum)
+    CHAT_SYSTEM(self.cc);
+  end
+
+  members.Pose = function(self, frame, slot, argStr, argNum)
+    control.Pose(self.cc);
+  end
+
+  members.Timer = function(self, frame, slot, argStr, argNum)
+    local timeHandleName = "SLOTMAC_TIMER_"..IMCRandom(1, 100000);
+    _G[timeHandleName] = function()
+      self.cc.act(self.cc, frame, slot, argStr, argNum);
+      _G[timeHandleName] = nil;
+    end
+    DebounceScript(timeHandleName, self.dd);
+  end
+
+  members.Equip = function(self, frame, slot, argStr, argNum)
+    local slotName, className = string.match(self.cc, "^(.-) (.-)$");
+    local invItem = session.GetInvItemByName(className);
+    item.Equip(slotName, invItem.invIndex);
+  end
+
+  members.UnEquip = function(self, frame, slot, argStr, argNum)
+    item.UnEquip(item.GetEquipSpotNum(self.cc));
+  end
+
+  return setmetatable(members, {__index = self});
+end
+-- set call.
+setmetatable(MacroCommand, {__call = MacroCommand.new});
