dc7ebe26608fe1d0e1343a9b4d61f04b5f223c79
= <dreamfragment623+weizlogy@gmail.com>
2017-07-18 00:18:39 +0900

    v1.1.0 objectdetector. fixed #4.

--- objectdetector/objectdetector.lua
+++ objectdetector/objectdetector.lua
@@ -3,36 +3,44 @@ ObjectDetector = {};
 function ObjectDetector.new(self)
   -- initialize members.
   local members = {};
-  members.Range = 700;
-  members.Lock = 0;
-  members.ObjectList = {};
+  members.range = 700;
+  members.lock = 0;
+  members.objectList = {};
+  members.path = "../addons/objectdetector/settings.txt";
+  members.config = {};
+
+  -- initialize class.
+  members.Initialize = function(self)
+    dofile(self.path);
+  end
+
   -- search all objects and output it.
   members.Update = function(self, frame, msg, str, myhandle)
-    if (self.Lock == 1) then
+    if (self.lock == 1) then
       return;
     end
-    self.Lock = 1;
-    ObjectList = {};
-    local list, count = SelectBaseObject(GetMyPCObject(), self.Range, "ALL");
+    self.lock = 1;
+    self.objectList = {};
+    local list, count = SelectBaseObject(GetMyPCObject(), self.range, "ALL");
     for i = 1 , count do
       local obj = list[i];
       local iesObj = GetBaseObjectIES(obj);
       local actor = tolua.cast(obj, "CFSMActor");
       local handle = actor:GetHandleVal();
-      ObjectList[handle] = 1;
+      self.objectList[handle] = 1;
       -- do nothing for my pc.
       if (myhandle ~= handle) then
         self:ShowDetectorObject(ui.GetFrame('map'), actor, iesObj);
         self:ShowDetectorObject(ui.GetFrame('minimap'), actor, iesObj);
       end
     end
-    self.Lock = 0;
+    self.lock = 0;
   end
   -- show object to maps.
   members.ShowDetectorObject = function(self, frame, actor, iesObj)
     -- check object info.
     local oi = self:GetDetectObjectInfo(actor, iesObj);
-    if (oi.IsVisible ~= 1) then
+    if (oi.isVisible ~= 1) then
       return;
     end
     local handle = actor:GetHandleVal();
@@ -48,18 +56,18 @@ function ObjectDetector.new(self)
     pic:SetImage("fullblack");
     pic:SetEnableStretch(1);
     pic:SetTooltipType('texthelp');
-    pic:SetTooltipArg(oi.TooltipText);
+    pic:SetTooltipArg(oi.tooltipText);
 
     local pictone = pic:CreateOrGetControl('picture', "tone", 1, 1, 5, 5);
     tolua.cast(pictone, "ui::CPicture");
     pictone:SetImage("fullwhite");
-    pictone:SetColorTone(oi.Color:ARGB());
+    pictone:SetColorTone(oi.color);
     pictone:SetEnableStretch(1);
     pictone:SetTooltipType('texthelp');
-    pictone:SetTooltipArg(oi.TooltipText);
+    pictone:SetTooltipArg(oi.tooltipText);
 
-    if (oi.IsBlink == 1) then
-      pictone:SetBlink(600000.0, 1.0, oi.Color:ARGB());
+    if (oi.isBlink == 1) then
+      pictone:SetBlink(600000.0, 1.0, oi.color);
     end
 
     pic:SetUserValue("HANDLE", handle);
@@ -73,66 +81,33 @@ function ObjectDetector.new(self)
   members.GetDetectObjectInfo = function(self, actor, iesObj)
     -- create object info.
     local oi = {
-      IsVisible = 1,
-      IsBlink = 0,
-      TooltipText = "...",
-      Color = {
-        A = "FF",
-        R = "00",
-        G = "00",
-        B = "00",
-        ARGB = function(self)
-          return self.A..self.R..self.G..self.B;
-        end
-      }
+      isVisible = 1,
+      isBlink = 0,
+      tooltipText = "...",
+      color = "FF000000"
     };
     -- insert info by object type, faction, and some conditions.
     local objType = actor:GetObjType();
     local faction = actor:GetFactionStr();
     if (objType == GT_MONSTER) then
       local rank = iesObj.MonRank;
-      oi.Color.R = "99";
-      oi.TooltipText = string.format("[Lv.%s]%s(%s) r=%s f=%s",
-       actor:GetLv(), actor:GetName(), iesObj.ClassName, rank, faction);
-      -- check faction.
-      if (faction == "Pet") then
-        oi.IsVisible = 0;
-        return oi;
-      elseif (faction == "RootCrystal") then
-        oi.Color.B = "99";
-      elseif (faction == "Neutral" or faction == "Peaceful" or faction == "Our_Forces" ) then
-        oi.Color.R = "FF";
-        oi.Color.G = "99";
-        -- check rank.
-        if (rank == "NPC" or rank == "MISC" or rank == "Normal" or rank == "Material") then
-          oi.Color.G = "66";
-        else
-          oi.Color.R = "00";
-          oi.Color.G = "00";
-          oi.Color.B = "00";
-        end
-      elseif (faction == "Monster") then
-        oi.Color.R = "66";
-      elseif (faction == "Monster_Chaos1" or faction == "Monster_Chaos2") then
-        oi.Color.R = "33";
-      elseif (faction == "Law") then
-        oi.Color.R = "FF";
-      elseif (faction == "IceWall") then
-        oi.Color.R = "00";
-        oi.Color.G = "00";
-        oi.Color.B = "00";
-      elseif (faction == "Hidden_tgt") then
-        oi.Color.R = "66";
-        oi.Color.G = "66";
-        oi.Color.B = "66";
-      else
-        oi.Color.R = "00";
-        oi.Color.G = "00";
-        oi.Color.B = "00";
+      local tempConfig = self.config.mon[faction];
+      if (tempConfig == nil) then
+        tempConfig = self.config.mon;
       end
+      oi.color = tempConfig.color;
+      oi.isVisible = tempConfig.isVisible;
+      oi.isBlink = tempConfig.isBlink;
+      oi.tooltipText = string.format("[Lv.%s]%s(%s) f=%s",
+       actor:GetLv(), actor:GetName(), iesObj.ClassName, faction);
       -- check treasure.
       if (string.find(iesObj.ClassName, "treasure") ~= nil) then
-        oi.IsBlink = 1;
+        local trConfig = self.config.treasure;
+        if (trConfig ~= nil) then
+          oi.color = trConfig.color;
+          oi.isVisible = trConfig.isVisible;
+          oi.isBlink = trConfig.isBlink;
+        end
       end
       -- check clover.
       local handle = actor:GetHandleVal();
@@ -141,22 +116,31 @@ function ObjectDetector.new(self)
 				local buff = info.GetBuffIndexed(handle, i);
         local cls = GetClassByType("Buff", buff.buffID);
         if (cls.Icon == "clover") then
+          oi.tooltipText = oi.tooltipText.." b="..cls.ClassName;
           --CHAT_SYSTEM(string.format("!!! clover !!! %d", buff.buffID));
-          oi.IsBlink = 1;
+          local clConfig = self.config.clover[cls.ClassName];
+          if (clConfig == nil) then
+            clConfig = self.config.clover;
+          end
+          oi.color = clConfig.color;
+          oi.isVisible = clConfig.isVisible;
+          oi.isBlink = clConfig.isBlink;
         end
 			end
     elseif (objType == GT_PC) then
-      oi.Color.B = "99";
-      oi.TooltipText = string.format("[Lv.%s]%s %s",
+      oi.color = self.config.pc.color;
+      oi.isVisible = self.config.pc.isVisible;
+      oi.tooltipText = string.format("[Lv.%s]%s %s",
        actor:GetLv(), actor:GetName(), actor:GetPCApc():GetFamilyName());
+--[[
     elseif (objType == GT_END) then
-      oi.Color.G = "99";
+      oi.color = self.config.end.color;
       CHAT_SYSTEM("GT_END=>"..type(iesObj));
+]]
     elseif (objType == GT_ITEM) then
-      oi.Color.R = "99";
-      oi.Color.G = "99";
-      oi.Color.B = "99";
-      oi.TooltipText = string.format("[Lv.%s]%s",
+      oi.color = self.config.item.color;
+      oi.isVisible = self.config.item.isVisible;
+      oi.tooltipText = string.format("[Lv.%s]%s",
        GET_ITEM_LEVEL(iesObj), actor:GetName());
     else
       -- CHAT_SYSTEM("unknown object type. => "..objType);
@@ -167,7 +151,7 @@ function ObjectDetector.new(self)
   members.UpdateObjectState = function(self, frame, parent, calculateAxis)
     local handle = frame:GetUserIValue("HANDLE");
     local actor = world.GetActor(handle);
-    if (ObjectList[handle] == nil or actor:IsDead() == 1) then
+    if (self.objectList[handle] == nil or actor:IsDead() == 1) then
       frame:ShowWindow(0);
       --ui.DestroyFrame(frame:GetName());
       return 1;
@@ -216,24 +200,31 @@ function ObjectDetector.new(self)
   end
   return setmetatable(members, {__index = self});
 end
+
 -- set call.
 setmetatable(ObjectDetector, {__call = ObjectDetector.new});
+
 -- frame initialize.
 function OBJECTDETECTOR_ON_INIT(addon, frame)
   -- clear objects.
   obde:Clear();
+  -- initialize.
+  obde:Initialize();
   -- regist check object handler.
   addon:RegisterMsg('MAP_CHARACTER_UPDATE', 'DETECTOR_UPDATE');
   addon:RegisterMsg('FPS_UPDATE', 'DETECTOR_UPDATE');
 end
+
 -- check object handler.
 function DETECTOR_UPDATE(frame, msg, str, handle)
   obde:Update(frame, msg, str, handle);
 end
+
 -- picture update handler.
 function UPDATE_MAP_STATE(frame)
   return obde:UpdateObjectState(frame, ui.GetFrame('map'), obde.CalculateMapAxis);
 end
+
 -- picture update handler.
 function UPDATE_MINIMAP_STATE(frame)
   return obde:UpdateObjectState(frame, ui.GetFrame('minimap'), obde.CalculateMinimapAxis);
