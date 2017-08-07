f3a2c192e2c4d1a75fc028756192bb1501e9b228
= <dreamfragment623+weizlogy@gmail.com>
2017-08-07 23:45:56 +0900

    v1.1.0 skillitemcounter.

--- skillitemcounter/skillitemcounter.lua
+++ skillitemcounter/skillitemcounter.lua
@@ -13,6 +13,13 @@ function SkillItemCounter.new(self)
     skillUseCount:SetText("{ol}{s12}"..math.floor(itemCount / skillCost));
     skillUseCount:SetOffset(slot:GetWidth() - skillUseCount:GetWidth(), 0);
   end
+  members.RefreshPoison = function(self, frame, index, skillCost, itemCount)
+		local slot = GET_CHILD_RECURSIVELY(frame, "slot"..(index + 1), "ui::CSlot");
+    local skillUseCount = slot:CreateOrGetControl('richtext', "skillUseCount", 0, 0, 20, 10);
+    tolua.cast(skillUseCount, "ui::CRichText");
+    skillUseCount:SetText("{ol}{s12}"..math.floor(itemCount / skillCost));
+    skillUseCount:SetOffset(slot:GetWidth() - skillUseCount:GetWidth(), 0);
+  end
   members.Destroy = function(self)
     QUICKSLOTNEXPBAR_ON_DROP = skic.QUICKSLOTNEXPBAR_ON_DROP;
     skic.QUICKSLOTNEXPBAR_ON_DROP = nil;
@@ -49,6 +56,11 @@ function SKILLITEMCOUNTER_REFRESH_COUNTER()
       if (skillitem ~= "None") then
         local invenItemInfo = session.GetInvItemByName(skillitem);
         skic:Refresh(frame, i, skl, invenItemInfo);
+      end
+      local skillPoison = SCR_Get_SpendPoison(skl);
+      if (skillPoison ~= 0) then
+        local etc = GetMyEtcObject();
+        skic:RefreshPoison(frame, i, skillPoison, etc.Wugushi_PoisonAmount);
       end
     else
       local slot = GET_CHILD_RECURSIVELY(frame, "slot"..(i + 1), "ui::CSlot");
