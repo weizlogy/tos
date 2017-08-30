2075465843517d1fd9f2307ef91f493c8feea660
= <dreamfragment623+weizlogy@gmail.com>
2017-08-31 01:20:41 +0900

    v1.1.0 dice.

--- dice/dice.lua
+++ dice/dice.lua
@@ -4,19 +4,35 @@ Dice = {};
 function Dice.new(self)
   -- initialize members.
   local members = {};
+  members.min = 0;
+  members.max = 999;
+
   -- throw dice.
   members.Throw = function(self)
     local server = GetServerNation();
     local name = GETMYFAMILYNAME();
-    local dice = IMCRandom(0, 999);
+    local dice = IMCRandom(self.min, self.max);
     if (server ~= "JP") then
-      return string.format("Dice!%s got %d on it!", name, dice);
+      return string.format("Dice(%d-%d)!%s got %d on it!", self.min, self.max, name, dice);
     end
-    return string.format("ダイス！%sは、%dを出した！", name, dice);
+    return string.format("ダイス(%d-%d)！%sは、%dを出した！", self.min, self.max, name, dice);
   end
+
+  members.ChangeRange = function(self, min, max)
+    if (min == nil) then
+      min = 0;
+    end
+    if (max == nil) then
+      max = 999;
+    end
+    self.min = min;
+    self.max = max;
+    CHAT_SYSTEM(string.format("[dice] set dice range %d - %d", min, max));
+  end
+
   -- destroy.
   members.Destroy = function(self)
-    UI_CHAT = dice.UI_CHAT;
+    --UI_CHAT = dice.UI_CHAT;
   end
   return setmetatable(members, {__index = self});
 end
@@ -31,6 +47,9 @@ function DICE_ON_INIT(addon, frame)
   UI_CHAT = function(msg)
     if (msg == "/random") then
       msg = dice:Throw();
+    elseif (string.find(msg, "/random set", 1, true) == 1) then
+      local min, max = string.match(msg, "^/random set (%d+) (%d+)$");
+      dice:ChangeRange(min, max);
     end
     dice.UI_CHAT(msg);
   end
