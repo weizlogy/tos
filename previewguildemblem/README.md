c13ab01cfc4b49ce240b49ffac5cc85afe000551
= <dreamfragment623+weizlogy@gmail.com>
2017-11-17 00:27:06 +0900

    support mini emblem preview.

--- previewguildemblem/README.md
+++ previewguildemblem/README.md
@@ -1,1 +1,7 @@
+### v1.1.0
+
+#### キャラ名横エンブレムプレビュー対応
+
+エンブレムの一覧（画像登録）画面でファイル名左にあるエンブレムアイコンをキャラ名横に表示されるサイズに合わせることでプレビュー可能としています。
+
 see [http://www.weizlogy.gq/tos/addon/previewguildemblem/](http://www.weizlogy.gq/tos/addon/previewguildemblem/) 
--- previewguildemblem/previewguildemblem.lua
+++ previewguildemblem/previewguildemblem.lua
@@ -7,6 +7,12 @@ function PreviewGuildEmblem.new(self)
 
   -- destroy.
   members.Destroy = function(self)
+    GUILDINFO_OPTION_INIT_EMBLEM = self.GUILDINFO_OPTION_INIT_EMBLEM;
+    self.GUILDINFO_OPTION_INIT_EMBLEM = nil;
+    GUILDEMBLEM_CHANGE_INIT = self.GUILDEMBLEM_CHANGE_INIT;
+    self.GUILDEMBLEM_CHANGE_INIT = nil;
+    SET_PRIVIEW_ITEM = self.SET_PRIVIEW_ITEM;
+    self.SET_PRIVIEW_ITEM = nil;
   end
   return setmetatable(members, {__index = self});
 end
@@ -35,11 +41,30 @@ function PREVIEWGUILDEMBLEM_ON_INIT(addon, frame)
   GUILDEMBLEM_CHANGE_INIT = function(frame)
     pgem.GUILDEMBLEM_CHANGE_INIT(frame);
     local frame = ui.GetFrame('guildemblem_change')
-    if frame ~= nil then
+    if frame ~= nil and AM_I_LEADER(PARTY_GUILD) == 0 then
       local acceptBtn = GET_CHILD_RECURSIVELY(frame, 'acceptBtn');
       acceptBtn:ShowWindow(0);
     end
   end
+
+  if (pgem.SET_PRIVIEW_ITEM == nil) then
+    pgem.SET_PRIVIEW_ITEM = SET_PRIVIEW_ITEM;
+  end
+  SET_PRIVIEW_ITEM = function(frame, ctrlSet, fileName, posY)
+    local gb_items = GET_CHILD(ctrlSet, "gb_items", "ui::CGroupBox");
+    local pic_icon = GET_CHILD(gb_items, "preview_icon", "ui::CPicture");
+    local pic_icon_edge = GET_CHILD(gb_items, "preview_icon_edge", "ui::CPicture");
+
+    local my = ui.GetFrame("charbaseinfo1_my");
+    local my_pic_icon = GET_CHILD(my, "guildEmblem", "ui::CPicture");
+    local my_pic_icon_edge = GET_CHILD(my, "guildEmblem_edge", "ui::CPicture");
+    
+    pic_icon:Resize(my_pic_icon:GetWidth(), my_pic_icon:GetHeight());
+    pic_icon_edge:Resize(my_pic_icon_edge:GetWidth(), my_pic_icon_edge:GetHeight());
+
+    return pgem.SET_PRIVIEW_ITEM(frame, ctrlSet, fileName, posY);
+  end
+
 end
 
 -- create instance.
