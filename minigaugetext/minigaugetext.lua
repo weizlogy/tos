c7f1ae8b24b3e53386be2db5444277f7632692cc
weizlogy <dreamfragment623+weizlogy@gmail.com>
2018-12-23 05:58:50 +0900

    minigaugetext v1.1.0

--- minigaugetext/minigaugetext.lua
+++ minigaugetext/minigaugetext.lua
@@ -12,28 +12,46 @@ local g = _G['ADDONS'][author][addonName]
 function g.new(self)
   local members = {}
 
+  local __ADDON_DIR = '../addons/'..addonName
+  local DEFAULT_SIZE = 11
+  local DEFAULT_COLOR = '#FFFFFF'
+
+  members.__config = {}
+
   -- HP/SP情報を更新する
   -- 本当はCGauge:AddStatでやりたいが、フォントサイズが12以下だと反映されない
   -- かつ、%vが100にしかならない？よくわからないξ･~ ･๑Ҙ
   members.Update = function(self, handle)
+    -- CHAT_SYSTEM('hadle = '..handle)
     local frame = ui.GetFrame("charbaseinfo1_"..handle)
+    if (not frame) then
+      return
+    end
     local hp = frame:GetChild("pcHpGauge")
     tolua.cast(hp, "ui::CGauge")
     local sp = frame:GetChild("pcSpGauge")
     tolua.cast(sp, "ui::CGauge")
 
+    local size = DEFAULT_SIZE
+    local color = DEFAULT_COLOR
+
     if (handle == 'my') then
       handle = session.GetMyHandle()
+      size = self.__config['size'] or DEFAULT_SIZE
+      color = self.__config['color'] or DEFAULT_COLOR
+    else
+      size = self.__config['ptsize'] or DEFAULT_SIZE
+      color = self.__config['ptcolor'] or DEFAULT_COLOR
     end
     local stat = info.GetStat(handle)
 
     local hptext = frame:CreateOrGetControl("richtext", "hptext", 0, 0, hp:GetWidth(), hp:GetHeight())
-    hptext:SetText(string.format('{s11}{ol}%d/%d', stat.HP, stat.maxHP))
+    hptext:SetText(string.format('{s%d}{ol}{%s}%d/%d', size, color, stat.HP, stat.maxHP))
     hptext:SetGravity(ui.RIGHT, ui.TOP);
-    hptext:SetOffset(hp:GetX(), hp:GetY() - 8)
+    hptext:SetOffset(hp:GetX(), hp:GetY() - 8 - (size - DEFAULT_SIZE))
 
     local sptext = frame:CreateOrGetControl("richtext", "sptext", 0, 0, sp:GetWidth(), sp:GetHeight())
-    sptext:SetText(string.format('{s11}{ol}%d/%d', stat.SP, stat.maxSP))
+    sptext:SetText(string.format('{s%d}{ol}{%s}%d/%d', size, color, stat.SP, stat.maxSP))
     sptext:SetGravity(ui.RIGHT, ui.TOP);
     sptext:SetOffset(sp:GetX(), sp:GetY() + 10)
 
@@ -44,8 +62,30 @@ function g.new(self)
     end
   end
 
+  members.LoadConfig = function(self)
+    self.__config = self:Deserialize('settings.txt') or {}
+  end
+
+  -- デシリアライズ
+  members.Deserialize = function(self, fileName)
+    local filePath = string.format('%s/%s', __ADDON_DIR, fileName)
+    local f, e = io.open(filePath, 'r')
+    if (e) then
+      return nil
+    end
+    f:close()
+    local s, e = pcall(dofile, filePath)
+    if (not s) then
+      CHAT_SYSTEM(e)
+    end
+    return e
+  end
+
   -- デストラクター
   members.Destroy = function(self)
+    if (self.UPDATE_PARTYINFO_HP ~= nil) then
+      UPDATE_PARTYINFO_HP = self.UPDATE_PARTYINFO_HP
+    end
   end
   -- おまじない
   return setmetatable(members, {__index = self})
@@ -55,10 +95,22 @@ setmetatable(g, {__call = g.new});
 
 -- 自フレーム初期化処理
 function MINIGAUGETEXT_ON_INIT(addon, frame)
+  g.instance:LoadConfig()
+
   addon:RegisterMsg('STAT_UPDATE', 'MINIGAUGETEXT_STAT_UPDATE');
   addon:RegisterMsg('TAKE_DAMAGE', 'MINIGAUGETEXT_STAT_UPDATE');
   addon:RegisterMsg('TAKE_HEAL', 'MINIGAUGETEXT_STAT_UPDATE');
   --addon:RegisterMsg('FPS_UPDATE', 'MINIGAUGETEXT_STAT_UPDATE_OTHERS');
+
+  -- PTメンバー表示用
+  if (g.instance.UPDATE_PARTYINFO_HP == nil) then
+    g.instance.UPDATE_PARTYINFO_HP = UPDATE_PARTYINFO_HP
+  end
+  UPDATE_PARTYINFO_HP = function(partyInfoCtrlSet, partyMemberInfo)
+    g.instance.UPDATE_PARTYINFO_HP(partyInfoCtrlSet, partyMemberInfo)
+    g.instance:Update(partyMemberInfo:GetHandle())
+  end
+
   MINIGAUGETEXT_STAT_UPDATE()
 end
 
