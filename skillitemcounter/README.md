c913ac1d120b615d3954a98b1a6a1d9eb9b48d7b
weizlogy <dreamfragment623+weizlogy@gmail.com>
2019-03-04 04:28:44 +0900

    ReBuild対応

--- skillitemcounter/README.md
+++ skillitemcounter/README.md
@@ -1,13 +1,7 @@
 ## Latest Release Note.
 
-### v1.1.1
+### v1.1.3
 
-#### 使用回数が表示されない問題の修正
-
-詳細は[#19](https://github.com/weizlogy/tos/issues/19)
-
-#### joypad対応
-
-joypadのquickslotに対応しました（遅
+- Re:Build対応
 
 see [http://www.weizlogy.gq/tos/addon/skillitemcounter/](http://www.weizlogy.gq/tos/addon/skillitemcounter/) 
--- skillitemcounter/addon_d.ipf/skillitemcounter/skillitemcounter.lua
+++ skillitemcounter/addon_d.ipf/skillitemcounter/skillitemcounter.lua
@@ -1,18 +1,38 @@
-SkillItemCounter = {};
+-- 領域定義
+local author = 'weizlogy'
+local addonName = 'skillitemcounter'
+_G['ADDONS'] = _G['ADDONS'] or {}
+_G['ADDONS'][author] = _G['ADDONS'][author] or {}
+_G['ADDONS'][author][addonName] = _G['ADDONS'][author][addonName] or {}
 
--- constructor.
-function SkillItemCounter.new(self)
+-- 領域へのポインターを取得
+local g = _G['ADDONS'][author][addonName]
+
+-- コンストラクター
+function g.new(self)
+
   -- initialize members.
   local members = {};
+
   members.Refresh = function(self, frame, index, skill, invItem)
 		local slot = GET_CHILD_RECURSIVELY(frame, "slot"..(index + 1), "ui::CSlot");
     local skillUseCount = slot:CreateOrGetControl('richtext', "skillUseCount", 0, 0, 20, 10);
     tolua.cast(skillUseCount, "ui::CRichText");
     local itemCount = invItem.count;
-    local skillCost = skill.SpendItemBaseCount + skill.SpendItemCount;
+
+    local skillCost = SCR_GET_SPENDITEM_COUNT(skill);
+    local skillName = string.match(skill.ClassName, '^.-%_(.+)')
+    local skillNameEsp = _G['SCR_GET_SPENDITEM_COUNT_'..skillName]
+    if (type(skillNameEsp) ~= 'nil') then
+      skillCost = skillNameEsp(skill)
+    end
+
+    self:Dbg(skillName..' '..itemCount..' '..skill.SpendItemBaseCount..' '..skill.SpendItemCount)
+
     skillUseCount:SetText("{ol}{s12}"..math.floor(itemCount / skillCost));
     skillUseCount:SetOffset(slot:GetWidth() - skillUseCount:GetWidth(), 0);
   end
+
   members.RefreshPoison = function(self, frame, index, skillCost, itemCount)
 		local slot = GET_CHILD_RECURSIVELY(frame, "slot"..(index + 1), "ui::CSlot");
     local skillUseCount = slot:CreateOrGetControl('richtext', "skillUseCount", 0, 0, 20, 10);
@@ -20,21 +40,34 @@ function SkillItemCounter.new(self)
     skillUseCount:SetText("{ol}{s12}"..math.floor(itemCount / skillCost));
     skillUseCount:SetOffset(slot:GetWidth() - skillUseCount:GetWidth(), 0);
   end
+
+  -- ログ出力
+  members.Dbg = function(self, msg)
+    -- CHAT_SYSTEM(string.format('[%s] <Dbg> %s', addonName, msg))
+  end
+  members.Log = function(self, msg)
+    CHAT_SYSTEM(string.format('[%s] <Log> %s', addonName, msg))
+  end
+  members.Err = function(self, msg)
+    CHAT_SYSTEM(string.format('[%s] <Err> %s', addonName, msg))
+  end
+
   members.Destroy = function(self)
-    QUICKSLOTNEXPBAR_ON_DROP = skic.QUICKSLOTNEXPBAR_ON_DROP;
-    skic.QUICKSLOTNEXPBAR_ON_DROP = nil;
+    QUICKSLOTNEXPBAR_ON_DROP = g.instance.QUICKSLOTNEXPBAR_ON_DROP;
+    g.instance.QUICKSLOTNEXPBAR_ON_DROP = nil;
   end
   return setmetatable(members, {__index = self});
 end
 -- set call.
-setmetatable(SkillItemCounter, {__call = SkillItemCounter.new});
+setmetatable(g, {__call = g.new})
+
 -- frame initialize.
 function SKILLITEMCOUNTER_ON_INIT(addon, frame)
-  if (skic.QUICKSLOTNEXPBAR_ON_DROP == nil) then
-    skic.QUICKSLOTNEXPBAR_ON_DROP = QUICKSLOTNEXPBAR_ON_DROP;
+  if (g.instance.QUICKSLOTNEXPBAR_ON_DROP == nil) then
+    g.instance.QUICKSLOTNEXPBAR_ON_DROP = QUICKSLOTNEXPBAR_ON_DROP;
   end
   QUICKSLOTNEXPBAR_ON_DROP = function(frame, control, argStr, argNum)
-    skic.QUICKSLOTNEXPBAR_ON_DROP(frame, control, argStr, argNum);
+    g.instance.QUICKSLOTNEXPBAR_ON_DROP(frame, control, argStr, argNum);
     SKILLITEMCOUNTER_REFRESH_KICKER();
   end
   addon:RegisterMsg('GAME_START_3SEC', 'SKILLITEMCOUNTER_REFRESH');
@@ -60,12 +93,12 @@ function SKILLITEMCOUNTER_REFRESH_COUNTER(frame)
       local skillitem = skl.SpendItem;
       if (skillitem ~= "None") then
         local invenItemInfo = session.GetInvItemByName(skillitem);
-        skic:Refresh(frame, i, skl, invenItemInfo);
+        g.instance:Refresh(frame, i, skl, invenItemInfo);
       end
       local skillPoison = SCR_Get_SpendPoison(skl);
       if (skillPoison ~= 0) then
         local etc = GetMyEtcObject();
-        skic:RefreshPoison(frame, i, skillPoison, etc.Wugushi_PoisonAmount);
+        g.instance:RefreshPoison(frame, i, skillPoison, etc.Wugushi_PoisonAmount);
       end
     else
       local slot = GET_CHILD_RECURSIVELY(frame, "slot"..(i + 1), "ui::CSlot");
@@ -74,9 +107,8 @@ function SKILLITEMCOUNTER_REFRESH_COUNTER(frame)
   end
 end
 
---
-if (skic ~= nil) then
-  skic:Destroy();
+-- インスタンス作成
+if (g.instance ~= nil) then
+  g.instance:Destroy()
 end
--- create instance.
-skic = SkillItemCounter();
+g.instance = g()
